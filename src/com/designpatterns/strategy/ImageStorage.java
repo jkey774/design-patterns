926433d1dac7520fdea5918a2de9fc365f8e2f4f
Joseph Key <jkey3@brookdale.com>
2020-12-31 01:39:14 -0600

    Strategy Pattern

---
+++ src/com/designpatterns/strategy/ImageStorage.java
@@ -0,0 +1,9 @@
+package com.designpatterns.strategy;
+
+public class ImageStorage {
+
+  public void store(String filename, Compressor compressor, Filter filter) {
+    compressor.compress(filename);
+    filter.apply(filename);
+  }
+}
--- src/com/designpatterns/strategy/Main.java
+++ src/com/designpatterns/strategy/Main.java
@@ -1,8 +1,15 @@
 package com.designpatterns.strategy;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
+        ImageStorage imageStorage = new ImageStorage();
+        imageStorage.store("a",
+                new JpegCompressor(), new BlackAndWhiteFilter());
+        imageStorage.store("a",
+                new PngCompressor(), new BlackAndWhiteFilter());
 
     }
 }
