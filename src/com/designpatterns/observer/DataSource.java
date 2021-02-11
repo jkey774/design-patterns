dbe0ddd8c893e3191c76925795059466d7a59a4d
Joseph Key <jkey3@brookdale.com>
2021-02-11 10:29:22 -0600

    observer pattern

---
+++ src/com/designpatterns/observer/DataSource.java
@@ -0,0 +1,16 @@
+package com.designpatterns.observer;
+
+// inherits all the capability for becoming a publisher
+public class DataSource extends Subject {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        notifyObservers();
+    }
+}
